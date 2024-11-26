8254797447d263c5c6ffb9afb8b5727ee67b718c
ZHADOW999 <babalolav9@gmail.com>
2024-11-27 00:02:58 +0100

    yh

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,24 @@ export default {
 
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "dark-mode-element":"var(--dark-mode-element)",
+        "dark-mode-bg":"var(--dark-mode-bg)",
+        "light-mode-text":"var(--light-mode-text)",
+        "light-mode-input":"var(--light-mode-input)",
+        "light-mode-bg":"var( --light-mode-bg)",
+        "dark-mode-text":"var(--dark-mode-text)",
+        "light-mode-element":"var(--light-mode-element)",
+      },
+
+      fontFamily:{
+        'Ns-condense':'Ns-condense',
+        'Ns-light':'Ns-light',
+        'Ns-medium':'Ns-medium',
+        'Ns-regular':'Ns-regular',
+      }
+    },
   },
   plugins: [
     flowbite.plugin(),
