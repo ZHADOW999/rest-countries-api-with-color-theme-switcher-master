494fc9bc3f550bc608b20c44f109baa275733bb8
ZHADOW999 <babalolav9@gmail.com>
2024-12-08 21:21:36 +0100

    finally !!!!!!!!!!!!!!!!!!!!!

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // You can add custom configuration here
+  build: {
+    // Example: specify output directory
+    outDir: 'dist',
+  },
+  // If you need to alias paths or configure plugins, you can do it here
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
 })
