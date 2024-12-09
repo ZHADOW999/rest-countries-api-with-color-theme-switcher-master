197c0eff380a43292fae66574c9f9f99d9b0a826
ZHADOW999 <babalolav9@gmail.com>
2024-12-09 18:08:56 +0100

    Update CountryDetails component to improve layout and user experience
    - Adjusted the section margin to enhance spacing and layout consistency.
    - Maintained existing styling and functionality while improving overall presentation.

--- src/pages/countryDetails.jsx
+++ src/pages/countryDetails.jsx
@@ -28,7 +28,7 @@ const CountryDetails = () => {
     {loading && <div className="bg-light-mode-bg dark:bg-dark-mode-bg flex justify-center items-center h-screen"><Spinner aria-label="Extra large spinner example" size="xl" /></div>}
     {error && <div className="bg-light-mode-bg dark:bg-dark-mode-bg text-light-mode-text dark:text-white  font-Ns-regular text-center text-2xl flex justify-center items-center ">{error}</div>}
       {country && country.length > 0 && (
-        <section className=" pt-14 bg-light-mode-bg dark:bg-dark-mode-bg min-h-screen transition-all duration-300 ease-in-out">
+        <section className="mb-20 pt-14 bg-light-mode-bg dark:bg-dark-mode-bg min-h-screen transition-all duration-300 ease-in-out">
           <div className=" dark:text-dark-mode-text text-light-mode-text m-auto w-[90%]">
             <div onClick={BackBtn}>
                 <button
